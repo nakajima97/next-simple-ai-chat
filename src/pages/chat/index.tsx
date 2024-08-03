72488cba0358f31290f5be88ef3bf7f48a62c1a5
nakajima97 <kituneudon97@gmail.com>
2024-08-03 14:17:39 +0900

    #1 会話画面のレイアウト調整

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,13 +1,25 @@
-import { Box } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 const Chat = () => {
 	return (
 		<Box sx={{ display: 'flex', height: '100vh' }}>
-			<Box sx={{width: '200px'}}>
-        ナビゲーションバー
+			<Box sx={{width: '200px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', backgroundColor: 'gray'}}>
+        <Box>
+          ChatApp
+        </Box>
+        <Box>
+        <Button variant='outlined'>ログアウト</Button>
+        </Box>
       </Box>
-			<Box sx={{ flexGrow: 1, backgroundColor: 'red', height: '100%' }}>
-				チャット画面
+			<Box sx={{ flexGrow: 1, height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Box>
+          <Typography variant='h5' sx={{borderBottom: '1px solid', padding: '10px'}}>Chat</Typography>
+        </Box>
+				<Box sx={{ flexGrow: 1 }}>会話履歴</Box>
+        <Box sx={{ display: 'flex', gap: 1, padding: '8px' }}>
+          <TextField label='メッセージを入力' sx={{ flexGrow: 1 }}/>
+          <Button variant='contained'>送信</Button>
+        </Box>
 			</Box>
 		</Box>
 	);
