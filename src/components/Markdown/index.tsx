b8dfe45b0430d26901ffe112fbe9088172ee6564
Nakajima <kituneudon97@gmail.com>
2024-08-17 12:35:52 +0900

    マークダウンへの対応 (#15)
    * #13 最低限マークダウンが表示されることを確認
    
    * #13 XSS対策
    
    * #13 formatterの実行
    
    * #13 リンター無視のコードを追加
    
    * #13 フォーマッターの実行

---
+++ src/components/Markdown/index.tsx
@@ -0,0 +1,23 @@
+import DOMPurify from 'dompurify';
+import { marked } from 'marked';
+import { useEffect, useState } from 'react';
+
+type Props = {
+	text: string;
+};
+
+export const Markdown = ({ text }: Props) => {
+	const [html, setHtml] = useState('');
+
+	useEffect(() => {
+		(async () => {
+			const clean = DOMPurify.sanitize(text);
+			const html = await marked(clean);
+			setHtml(html);
+		})();
+	}, [text]);
+
+	// HTML文字列をそのまま表示するため、dangerouslySetInnerHTMLを使用
+	// biome-ignore lint: lint/security/noDangerouslySetInnerHtml
+	return <div dangerouslySetInnerHTML={{ __html: html }} />;
+};
