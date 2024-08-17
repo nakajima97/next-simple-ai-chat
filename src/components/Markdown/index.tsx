f656bf3e466c3f9a0f51bb88752a57d5904772c9
nakajima97 <kituneudon97@gmail.com>
2024-08-17 12:04:11 +0900

    #13 XSS対策

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,13 +1,23 @@
-import { Box } from "@mui/material";
 import { marked } from "marked";
+import DOMPurify from 'dompurify';
+import { useEffect, useState } from "react";
 
 type Props = {
   text: string
 }
 
 export const Markdown = ({text}: Props) => {
-  const html = marked(text);
+  const [html, setHtml] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const clean = DOMPurify.sanitize(text);
+      const html = await marked(clean);
+      setHtml(html);
+  })();
+  }, [text]);
+
   return (
-    <Box dangerouslySetInnerHTML={{ __html: html }} />
+    <div dangerouslySetInnerHTML={{ __html: html }} />
   );
 }
