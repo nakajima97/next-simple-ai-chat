1adfa70c30e82e1444e43577c8642fc6d3c56cf5
nakajima97 <kituneudon97@gmail.com>
2024-08-17 16:03:31 +0900

    #22 ctrl + enterで送信する機能を付けた

--- src/features/chat/components/ui/ChatForm/index.tsx
+++ src/features/chat/components/ui/ChatForm/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Typography, useTheme } from '@mui/material';
 import { type FC, useState } from 'react';
 
 type Props = {
@@ -12,18 +12,30 @@ export const ChatForm: FC<Props> = ({
 	handleSendMessage,
 	message,
 }) => {
+  const theme = useTheme();
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter' && e.ctrlKey) {
+			handleSendMessage();
+		}
+	};
+
 	return (
-		<Box sx={{ display: 'flex', gap: 1, padding: '8px' }}>
-			<TextField
-				label="メッセージを入力"
-				sx={{ flexGrow: 1 }}
-				onChange={handleChangeMessage}
-				value={message}
-				multiline
-			/>
-			<Button variant="contained" onClick={handleSendMessage}>
-				送信
-			</Button>
+		<Box>
+			<Typography align='right' sx={{fontSize: '12px', paddingRight: '16px', color: theme.palette.secondary.dark }}>ctrl + enterで送信</Typography>
+			<Box sx={{ display: 'flex', gap: 1, padding: '8px' }}>
+				<TextField
+					label="メッセージを入力"
+					sx={{ flexGrow: 1 }}
+					value={message}
+					multiline
+					onChange={handleChangeMessage}
+					onAbort={handleKeyDown}
+				/>
+				<Button variant="contained" onClick={handleSendMessage}>
+					送信
+				</Button>
+			</Box>
 		</Box>
 	);
 };
