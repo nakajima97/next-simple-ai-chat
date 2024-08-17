abea67717b05bf0ee1d3c853f5d4d1845d2d50de
nakajima97 <kituneudon97@gmail.com>
2024-08-17 12:20:24 +0900

    #13 formatterの実行

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,23 +1,21 @@
-import { marked } from "marked";
 import DOMPurify from 'dompurify';
-import { useEffect, useState } from "react";
+import { marked } from 'marked';
+import { useEffect, useState } from 'react';
 
 type Props = {
-  text: string
-}
+	text: string;
+};
 
-export const Markdown = ({text}: Props) => {
-  const [html, setHtml] = useState('');
+export const Markdown = ({ text }: Props) => {
+	const [html, setHtml] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      const clean = DOMPurify.sanitize(text);
-      const html = await marked(clean);
-      setHtml(html);
-  })();
-  }, [text]);
+	useEffect(() => {
+		(async () => {
+			const clean = DOMPurify.sanitize(text);
+			const html = await marked(clean);
+			setHtml(html);
+		})();
+	}, [text]);
 
-  return (
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-  );
-}
+	return <div dangerouslySetInnerHTML={{ __html: html }} />;
+};
