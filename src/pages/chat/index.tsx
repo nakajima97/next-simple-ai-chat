aa505aafb3d8c8fb2fe05c6f8764824d4c02c898
Nakajima <kituneudon97@gmail.com>
2024-08-24 11:23:36 +0900

    コンソールエラーの解消 (#35)
    * #33 ログイン済みユーザがトップ画面にアクセスした際のエラー解消
    
    * #33 不要なコンソールログの削除
    
    * #33 playwrightのテストコードを書く予定はないので削除
    
    * #33 biomeのエラー解消

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,12 +1,15 @@
 import CircularIndeterminate from '@/components/PageLoader';
 import ChatPage from '@/features/chat/components/page/ChatPage';
 import { useProtectedRoute } from '@/hooks/useProtectedRoute';
+import { useEffect } from 'react';
 
 const chat = () => {
 	const { redirectToLogin, isLoading } = useProtectedRoute();
 
 	// ログインしていない場合はログインページにリダイレクトする
-	redirectToLogin();
+	useEffect(() => {
+		redirectToLogin();
+	}, [redirectToLogin]);
 
 	if (isLoading) {
 		return <CircularIndeterminate />;
