03152d328bf21832a4739f3fcbfaa87dda69788c
nakajima97 <kituneudon97@gmail.com>
2024-08-31 13:59:20 +0900

    #27 新規登録時のエラーメッセージ修正

--- src/features/register/components/ui/RegisterForm/index.tsx
+++ src/features/register/components/ui/RegisterForm/index.tsx
@@ -1,3 +1,4 @@
+import { AlertContext } from '@/context/AlertContext';
 import { getAuth } from '@/libs/firebase/firebase';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import {
@@ -9,14 +10,16 @@ import {
 	TextField,
 	Typography,
 } from '@mui/material';
+import { FirebaseError } from 'firebase/app';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import type React from 'react';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 
 export const RegisterForm = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const { showAlert } = useContext(AlertContext);
 
 	const router = useRouter();
 
@@ -24,15 +27,27 @@ export const RegisterForm = () => {
 
 	const handleSubmit = async () => {
 		try {
-			const credential = await createUserWithEmailAndPassword(
+			await createUserWithEmailAndPassword(
 				auth,
 				email,
 				password,
 			);
 
+			showAlert({ message: 'ログインしました', severity: 'success' });
 			router.push('/chat');
-		} catch (error) {
-			console.error(error);
+		} catch (error: unknown) {
+			if (error instanceof FirebaseError) {
+				console.error(error);
+				console.log({error});
+				console.log(error.code);
+				if (error.code === 'auth/email-already-in-use') {
+					showAlert({ message: 'このメールアドレスは既に登録されています。', severity: 'error' });
+				} else if (error.code === 'auth/invalid-email') {
+					showAlert({ message: 'メールアドレスの形式が正しくありません。', severity: 'error' });
+				} else {
+					showAlert({ message: 'エラーが発生して登録に失敗しました。', severity: 'error' });
+				}
+			}
 		}
 	};
 
